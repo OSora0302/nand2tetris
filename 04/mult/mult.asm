1d491f6946eabb7b95ed7f1a7f86001b6f4ed410
OSora0302 <63058025+OSora0302@users.noreply.github.com>
2020-11-01 21:22:53 +0900

    Implement mult.asm

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,33 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// Add R0 to R2 (R1 times)
+
+@R2
+M=0
+
+(LOOP)
+
+@R1
+D=M
+
+@END
+D;JEQ
+
+@R0
+D=M
+
+@R2
+M=M+D
+
+@R1
+M=M-1
+
+@LOOP
+0;JMP
+
+(END)
+
+@END
+0;JMP
